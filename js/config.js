271e6c1d89078e532c2f95e14cf4403120326c69
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2025-01-07 22:13:25 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://i7mew86yk9.execute-api.us-east-2.amazonaws.com/dev',
     }
 };
